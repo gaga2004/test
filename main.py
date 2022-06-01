13cedb8ca585286706801ff3cb5a232ebe9f53a9
Joshua King <105618261+gaga2004@users.noreply.github.com>
2022-06-01 23:07:03 +0000

    shit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("chur g")
+#Import required libraries
+import sys
+from PIL import Image, ImageDraw
+
+#Create Image object
+im = Image.open("images/logo.jpg")
+
+#Draw line
+draw = ImageDraw.Draw(im)
+draw.line((0, 0) + im.size, fill=128)
+draw.line((0, im.size[1], im.size[0], 0), fill=128)
+
+#Show image
+im.show()
